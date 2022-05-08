e5875019833907576e20b614026f9cb5b69d7086
tomkuzma <77029857+tomkuzma@users.noreply.github.com>
2022-05-07 22:28:15 -0700

    added comments

--- lib/SDCard/SDCard.h
+++ lib/SDCard/SDCard.h
@@ -5,12 +5,31 @@
 #include "FS.h"
 #include "SD.h"
 
-// returns list of filenames as a string
+/////////////////// listDir /////////////////////////////////////////////////////////////////////////
+// returns list of filenames as a string. Deals with hidden files created from Apple/Linux systems.
+// Args:    fs      - FS object
+//          dirname - file path
+//          levels  - # of levels to look into 
+// Returns: list of filenames as a string.
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 String listDir(fs::FS &fs, const char *dirname, uint8_t levels);
 
+/////////////////// scanFile /////////////////////////////////////////////////////////////////////////
+// Scans text file and indexes the position location of all the lines in the file.
+// Args:    fs      - FS object
+//          path    - file path
+// Returns: number of lines in text file. -1 if file read error. 
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 int scanFile(fs::FS &fs, const char *path);
 
-void readLines(fs::FS &fs, int lineNum, int lineCount, const char *path);
-
+/////////////////// readLines ///////////////////////////////////////////////////////////////////////
+// Goes to position keyed from lineNum arg and returns 3 lines as a string from file. 
+// Args:    fs          - FS object
+//          path        - file path
+//          lineNum     - line in file to start read
+//          lineCount   - total lines to deal with scrolling max
+// Returns: String of desired 3 lines from text file
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+String readLines(fs::FS &fs, int lineNum, int lineCount, const char *path);
 
 #endif // __SDCARD_H__
